81f60220e0d230f2559012c2c41de2f0554d33a9
Trevor DePew <t.depewmusic@gmail.com>
2024-11-22 14:49:55 -0500

    further setup configurations for deploy

--- core/settings.py
+++ core/settings.py
@@ -22,17 +22,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l9#$hbri#iw+l0pr@015ny&_+vouxsvi^bl3cb5()(mf^#p_gk'
+SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-l9#$hbri#iw+l0pr@015ny&_+vouxsvi^bl3cb5()(mf^#p_gk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
 
 INSTALLED_APPS = [
+    'swportfolio.apps.SwportfolioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
